358533d67b15d86741a3fee1a4978d3bd2208824
YoucefDjellas10 <djellasyoucef10@gmail.com>
2025-03-09 06:45:27 +0000

    05-09 /3

--- crud/settings.py
+++ crud/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Permet de reconnaître le dossier static global
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
