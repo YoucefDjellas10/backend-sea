b7b505ca7e3a786e061b0e14b28db21300b122a9
YoucefDjellas10 <djellasyoucef10@gmail.com>
2025-11-01 00:32:16 +0100

    lazem nkemel

--- crud/settings.py
+++ crud/settings.py
@@ -9,7 +9,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'ssl0.ovh.net'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'no-reply@safarelamir.com'
 EMAIL_HOST_PASSWORD = 'crbu ewvz agke cyt'
