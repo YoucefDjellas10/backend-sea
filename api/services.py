9ce45e8708d398080416ddca84ceca4be221a6a1
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-01-18 12:25:36 +0100

    after

--- api/services.py
+++ api/services.py
@@ -723,8 +723,7 @@ def verify_and_calculate(ref, lieu_depart, lieu_retour, date_depart, heure_depar
             is_available = True
 
             for reservation in vehicle_reservations:
-                if reservation.name == "269991":
-                    print("////////// 269991 est la ///////////")
+                print("########## reservation : ", reservation.name)
                 if (date_depart_heure < reservation.date_heure_fin and date_retour_heure > reservation.date_heure_debut and ref != record.name):
                     is_available = False
                     break
