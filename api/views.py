b3c4ca5c438ef81c5b620f632e1d605d520c6313
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-01-20 11:38:18 +0100

    after

--- api/views.py
+++ api/views.py
@@ -2643,7 +2643,9 @@ def verify_and_do(ref, lieu_depart, lieu_retour, date_depart, heure_depart, date
         
         if not verify_value[0].get('is_available') or not verify_value[0].get('old_total') or not verify_value[0].get('new_total'):
             return {"success": "no"}
-        
+
+        user = Users.objects.get(id=did_by)
+
         print("********** ref : ",ref)
         print("********** lieu_depart : ",lieu_depart)
         print("********** lieu_retour : ",lieu_retour)
@@ -2653,8 +2655,6 @@ def verify_and_do(ref, lieu_depart, lieu_retour, date_depart, heure_depart, date
         print("********** heure_retour : ",heure_retour)
         print("********** backoffice : ",backoffice)
         print("********** did_by : ",did_by)
-
-        user = Users.objects.get(id=did_by)
 
         if verify_value and verify_value[0].get('is_available') == "yes":
             reservation_obj = Reservation.objects.get(name=ref)
