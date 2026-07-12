1031726206e3d107fc2e3e7b09fb6d839c180717
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-07-12 15:42:38 +0100

    after

--- api/views.py
+++ api/views.py
@@ -3055,6 +3055,7 @@ def verify_and_do(ref, lieu_depart, lieu_retour, date_depart, heure_depart, date
                             caution.save()
         
                 if reservation_obj.lieu_depart != lieu_depart_obj or reservation_obj.lieu_retour != lieu_retour_obj :
+                    print("######### changement lieux  #########")
                     reservation_obj.ancien_lieu = f"{reservation_obj.lieu_depart.name} → {reservation_obj.lieu_retour.name}"
                     reservation_obj.depart_retour = f"{lieu_depart_obj.name} → {lieu_retour_obj.name}"
                     reservation_obj.lieu_depart = lieu_depart_obj
