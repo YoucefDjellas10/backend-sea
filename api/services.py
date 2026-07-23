13a5c6c1ac4faee4bcc43c3722cc6e20487890c1
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-07-23 22:13:35 +0100

    after

--- api/services.py
+++ api/services.py
@@ -1508,7 +1508,7 @@ def otp_verify(email, otp, client_id):
         
         otp_time += timedelta(hours=1)
 
-        if str(client.otp) == str(otp) and timezone.now() - otp_time < timedelta(minutes=1):
+        if str(client.otp) == str(otp) and timezone.now() - otp_time < timedelta(minutes=5):
             client.otp = None
             client.otp_created_at = None
             client.otp_attempts = 0
@@ -1520,7 +1520,7 @@ def otp_verify(email, otp, client_id):
             client.otp_attempts = 0
             client.save()
             return {"success": False, "expired":True, "extra_attempts": True}
-        elif (str(client.otp) == str(otp) and timezone.now() - otp_time > timedelta(minutes=1)):
+        elif (str(client.otp) == str(otp) and timezone.now() - otp_time > timedelta(minutes=5)):
             client.otp = None
             client.otp_created_at = None
             client.otp_attempts = 0
