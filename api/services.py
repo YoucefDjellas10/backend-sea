07f568e3b2799b7cf57b49c974ef11186f7c8b86
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-07-27 09:34:05 +0100

    after

--- api/services.py
+++ api/services.py
@@ -1131,6 +1131,8 @@ def verify_and_calculate(ref, lieu_depart, lieu_retour, date_depart, heure_depar
                   f"refund_amount={refund_amount}, old_total={old_total}, new_total={new_total}, "
                   f"frais={frais}, credit={credit}, credit_amount={credit_amount}")
 
+            remaining_to_pay = round(new_total - float(record.montant_paye), 2) if country_code != "DZ" else round(new_total - (float(record.montant_paye) * taux_change), 2)
+
             result.append({
                 'is_available': "yes",
                 "payment_required": payment_required,
@@ -1145,7 +1147,7 @@ def verify_and_calculate(ref, lieu_depart, lieu_retour, date_depart, heure_depar
                 'old_total': old_total,
                 'new_total': new_total,
                 'frais': frais,
-                'amount_paid': round(record.montant_paye, 2),
+                'amount_paid': round(record.montant_paye, 2) if country_code != "DZ" else round(record.montant_paye, 2) * taux_change,
                 'remaining_to_pay': round(new_total - float(record.montant_paye), 2),
                 "credit": credit,
                 "credit_amount": credit_amount
