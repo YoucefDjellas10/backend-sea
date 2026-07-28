adf0b5c34b4f580953a95620708bf6e718282407
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-07-28 15:31:55 +0100

    after

--- api/services.py
+++ api/services.py
@@ -809,6 +809,8 @@ def verify_and_calculate(ref, lieu_depart, lieu_retour, date_depart, heure_depar
 
             # ── Promotions (même logique que search_result_vehicule) ──────────
             promotions = Promotion.objects.filter(
+                debut_visibilite__lte=today,
+                fin_visibilite__gte=today,
                 date_debut__lte=date_retour_obj,
                 date_fin__gte=date_depart_obj,
                 active_passive=True
